8408cb6d99fb42c600c2ad7075817e07826caa5a
Colin MacDonald <colin@goinstant.com>
2014-07-29 20:00:16 -0300

    Tutorial 01

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-unity_tutorials
-===============
+#### Unity Tutorials
 
-Unity Tutorials
+![](http://unity3d.com/profiles/unity3d/themes/unity/images/company/brand/logos/primary/unity.jpg)
---
+++ tutorial_01/Assets/Scripts/BlockStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockStart : MonoBehaviour {
+
+  void OnMouseDown () {
+    renderer.material.color = Color.blue;
+  }
+
+  void OnMouseUp () {
+    renderer.material.color = Color.white;
+  }
+}
